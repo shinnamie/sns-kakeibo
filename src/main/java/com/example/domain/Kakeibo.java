958ae11063bd3f2eff2c76b4c4580a5c2de28fc0
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-09-16 14:55:25 +0900

    家計簿一覧表示まで完了

--- src/main/java/com/example/domain/Kakeibo.java
+++ src/main/java/com/example/domain/Kakeibo.java
@@ -13,7 +13,7 @@ public class Kakeibo {
 	/** 費目Id */
 	private Integer expenseItemId;
 	/** 支出金額 */
-	private Integer expendituerAmount;
+	private Integer expenditureAmount;
 	/** 収入金額 */
 	private Integer incomeAmount;
 	/** 決済Id */
@@ -26,101 +26,81 @@ public class Kakeibo {
 	private LocalDate insertDate;
 	/** 更新日 */
 	private LocalDate updateDate;
-
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
 	public Integer getUserId() {
 		return userId;
 	}
-
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
-
 	public LocalDate getSettlementDate() {
 		return settlementDate;
 	}
-
 	public void setSettlementDate(LocalDate settlementDate) {
 		this.settlementDate = settlementDate;
 	}
-
 	public Integer getExpenseItemId() {
 		return expenseItemId;
 	}
-
 	public void setExpenseItemId(Integer expenseItemId) {
 		this.expenseItemId = expenseItemId;
 	}
 
-	public Integer getExpendituerAmount() {
-		return expendituerAmount;
+	public Integer getExpenditureAmount() {
+		return expenditureAmount;
 	}
 
-	public void setExpendituerAmount(Integer expendituerAmount) {
-		this.expendituerAmount = expendituerAmount;
+	public void setExpenditureAmount(Integer expenditureAmount) {
+		this.expenditureAmount = expenditureAmount;
 	}
-
 	public Integer getIncomeAmount() {
 		return incomeAmount;
 	}
-
 	public void setIncomeAmount(Integer incomeAmount) {
 		this.incomeAmount = incomeAmount;
 	}
-
 	public Integer getSettlementId() {
 		return settlementId;
 	}
-
 	public void setSettlementId(Integer settlementId) {
 		this.settlementId = settlementId;
 	}
-
 	public String getUsedStore() {
 		return usedStore;
 	}
-
 	public void setUsedStore(String usedStore) {
 		this.usedStore = usedStore;
 	}
-
 	public String getRemarks() {
 		return remarks;
 	}
-
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
 	}
-
 	public LocalDate getInsertDate() {
 		return insertDate;
 	}
-
 	public void setInsertDate(LocalDate insertDate) {
 		this.insertDate = insertDate;
 	}
-
 	public LocalDate getUpdateDate() {
 		return updateDate;
 	}
-
 	public void setUpdateDate(LocalDate updateDate) {
 		this.updateDate = updateDate;
 	}
-
 	@Override
 	public String toString() {
 		return "Kakeibo [id=" + id + ", userId=" + userId + ", settlementDate=" + settlementDate + ", expenseItemId="
-				+ expenseItemId + ", expendituerAmount=" + expendituerAmount + ", incomeAmount=" + incomeAmount
+				+ expenseItemId + ", expenditureAmount=" + expenditureAmount + ", incomeAmount=" + incomeAmount
 				+ ", settlementId=" + settlementId + ", usedStore=" + usedStore + ", remarks=" + remarks
 				+ ", insertDate=" + insertDate + ", updateDate=" + updateDate + "]";
 	}
+
 
 }
