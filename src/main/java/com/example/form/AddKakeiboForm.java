bf2b0a9f905024cf06f2bd0caf17f25575219d3b
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-09-20 09:21:10 +0900

    カラム名を変更したことにより、insert処理の方のバインド変数及びKakeiboFormのプロパティを修正

--- src/main/java/com/example/form/AddKakeiboForm.java
+++ src/main/java/com/example/form/AddKakeiboForm.java
@@ -5,7 +5,6 @@ import java.sql.Timestamp;
 
 import javax.validation.constraints.NotNull;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
 import com.example.domain.ExpenseItem;
@@ -18,8 +17,6 @@ public class AddKakeiboForm {
 	/** ユーザーId */
 	private Integer userId;
 	/** 決済日付 */
-	@DateTimeFormat(pattern = "yyyy/MM/dd")
-	@NotNull(message = "日付を指定してください")
 	private Date settlementDate;
 	/** 費目Id */
 	private Integer expenseItemId;
@@ -38,9 +35,9 @@ public class AddKakeiboForm {
 	/** 備考 */
 	private String remarks;
 	/** 登録日 */
-	private Timestamp insertDate;
+	private Timestamp insertAt;
 	/** 更新日 */
-	private Timestamp updateDate;
+	private Timestamp updateAt;
 	/** 費目 */
 	private ExpenseItem expenseItem;
 	/** 決済 */
@@ -63,12 +60,15 @@ public class AddKakeiboForm {
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
@@ -80,73 +80,94 @@ public class AddKakeiboForm {
 	public void setSettlementDate(Date settlementDate) {
 		this.settlementDate = settlementDate;
 	}
+
 	public Integer getExpenseItemId() {
 		return expenseItemId;
 	}
+
 	public void setExpenseItemId(Integer expenseItemId) {
 		this.expenseItemId = expenseItemId;
 	}
+
 	public Integer getExpenditureAmount() {
 		return expenditureAmount;
 	}
+
 	public void setExpenditureAmount(Integer expenditureAmount) {
 		this.expenditureAmount = expenditureAmount;
 	}
+
 	public Integer getIncomeAmount() {
 		return incomeAmount;
 	}
+
 	public void setIncomeAmount(Integer incomeAmount) {
 		this.incomeAmount = incomeAmount;
 	}
+
 	public Integer getSettlementId() {
 		return settlementId;
 	}
+
 	public void setSettlementId(Integer settlementId) {
 		this.settlementId = settlementId;
 	}
+
 	public String getUsedStore() {
 		return usedStore;
 	}
+
 	public void setUsedStore(String usedStore) {
 		this.usedStore = usedStore;
 	}
+
 	public String getRemarks() {
 		return remarks;
 	}
+
 	public void setRemarks(String remarks) {
 		this.remarks = remarks;
 	}
-	public Timestamp getInsertDate() {
-		return insertDate;
+
+	public Timestamp getInsertAt() {
+		return insertAt;
 	}
-	public void setInsertDate(Timestamp insertDate) {
-		this.insertDate = insertDate;
+
+	public void setInsertAt(Timestamp insertAt) {
+		this.insertAt = insertAt;
 	}
-	public Timestamp getUpdateDate() {
-		return updateDate;
+
+	public Timestamp getUpdateAt() {
+		return updateAt;
 	}
-	public void setUpdateDate(Timestamp updateDate) {
-		this.updateDate = updateDate;
+
+	public void setUpdateAt(Timestamp updateAt) {
+		this.updateAt = updateAt;
 	}
+
 	public ExpenseItem getExpenseItem() {
 		return expenseItem;
 	}
+
 	public void setExpenseItem(ExpenseItem expenseItem) {
 		this.expenseItem = expenseItem;
 	}
+
 	public Settlement getSettlement() {
 		return settlement;
 	}
+
 	public void setSettlement(Settlement settlement) {
 		this.settlement = settlement;
 	}
+
 	@Override
 	public String toString() {
 		return "AddKakeiboForm [id=" + id + ", userId=" + userId + ", settlementDate=" + settlementDate
 				+ ", expenseItemId=" + expenseItemId + ", expenditureAmount=" + expenditureAmount + ", incomeAmount="
 				+ incomeAmount + ", settlementId=" + settlementId + ", usedStore=" + usedStore + ", remarks=" + remarks
-				+ ", insertDate=" + insertDate + ", updateDate=" + updateDate + ", expenseItem=" + expenseItem
-				+ ", settlement=" + settlement + "]";
+				+ ", insertAt=" + insertAt + ", updateAt=" + updateAt + ", expenseItem=" + expenseItem + ", settlement="
+				+ settlement + "]";
 	}
 
 
