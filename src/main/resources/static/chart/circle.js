b17e0af4d09ed14b4d1f4c26df1ffe2c34f738b2
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-09-27 17:28:56 +0900

    収支内訳割合をもとに円グラフの作成が完了

--- src/main/resources/static/chart/circle.js
+++ src/main/resources/static/chart/circle.js
@@ -1,20 +1,114 @@
 "use strict";
 
 window.onload = function () {
+	// 費目の配列データ
+	let expenseItemNames = document.getElementsByClassName('expenseItemNameList');
+	// 費目ごとの合計金額の配列データ
+	let totalByExpenseItems = document.getElementsByClassName('totalByExpenseItemList');
+	// 割合の配列データ
+	let rates = document.getElementsByClassName('rateList');
+	
+	// 本月で集計された「費目名」を新しい配列を作成し格納する
+	let newNames = [];
+	for (let expenseItemName of expenseItemNames) {
+		newNames.push(expenseItemName.innerText);
+	}
+	
+	// 本月で集計された費目名ごとの「割合」を新しい配列を作成し格納する
+	let newRates = [];
+	for (let rate of rates) {
+		newRates.push(rate.innerText);
+	}
+	
+	// グラフの色を判定し新しい配列に格納する関数
+	let newColor = [];
+	for (let expenseItemName of newNames) {
+		switch(expenseItemName) {
+			case "食費":
+				newColor.push("#fa8072");
+				break;
+			case "日用品":
+				newColor.push("#00ff7f");
+				break;
+			case "趣味・娯楽":
+				newColor.push("#ff1493");
+				break;
+			case "交際費":
+				newColor.push("#1e90ff");
+				break;
+			case "交通費":
+				newColor.push("#9400d3");
+				break;
+			case "衣服":
+				newColor.push("#FFFF00");
+				break;
+			case "美容":
+				newColor.push("#ffd700");
+				break;
+			case "健康・医療":
+				newColor.push("#ffa07a");
+				break;
+			case "自動車":
+				newColor.push("#c0c0c0");
+				break;
+			case "教養・教育":
+				newColor.push("#6495ed");
+				break;
+			case "水道・光熱費":
+				newColor.push("#00bfff");
+				break;
+			case "通信費":
+				newColor.push("#9932cc");
+				break;
+			case "住宅":
+				newColor.push("#32cd32");
+				break;
+			case "税・社会保障":
+				newColor.push("#ffebcd");
+				break;
+			case "保険":
+				newColor.push("#ff69b4");
+				break;
+			case "その他":
+				newColor.push("#fffacd");
+				break;	
+		}
+		//if (expenseItemName == "食費") newColor.push("#fa8072");
+		//if (expenseItemName == "日用品") newColor.push("#00ff7f");
+		//if (expenseItemName == "趣味・娯楽") newColor.push("#ff1493");
+		//if (expenseItemName == "交際費") newColor.push("#1e90ff");
+		//if (expenseItemName == "交通費") newColor.push("#9400d3");
+		//if (expenseItemName == "衣服") newColor.push("#FFFF00");
+		//if (expenseItemName == "美容") newColor.push("#ffd700");
+		//if (expenseItemName == "健康・医療") newColor.push("#ffa07a");
+		//if (expenseItemName == "自動車") newColor.push("#c0c0c0");
+		//if (expenseItemName == "教養・教育") newColor.push("#6495ed");
+		//if (expenseItemName == "水道・光熱費") newColor.push("#00bfff");
+		//if (expenseItemName == "通信費") newColor.push("#9932cc");
+		//if (expenseItemName == "住宅") newColor.push("#32cd32");
+		//if (expenseItemName == "税・社会保障") newColor.push("#ffebcd");
+		//if (expenseItemName == "保険") newColor.push("#ff69b4");
+		//if (expenseItemName == "その他") newColor.push("#fffacd");
+	}
+	
+	// 円グラフの情報取得
 	let context = document.querySelector("#balance_circle").getContext('2d')
     new Chart(context, {
+	  // データの種類
       type: 'pie',
+      // データの設定
       data: {
-        labels: ["食費", "日用品", "趣味・娯楽", "交際費", "交通費", "衣服", "美容", "健康・医療", "自動車", "教養・教育", "水道・光熱費", "通信費", "住宅", "税・社会保障", "保険", "その他"],
+        labels: newNames,
         datasets: [{
-          backgroundColor: ["#fa8072", "#00ff7f", "#ff1493", "#1e90ff", "#9400d3", "#FFFF00", "#ffd700", "#ffa07a", "#c0c0c0", "#6495ed", "#00bfff", "#9932cc", "#32cd32", "#ffebcd", "#ff69b4", "#fffacd"],
-          data: [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
+         	backgroundColor: newColor,
+          	data: newRates
         }]
       },
       options: {
         responsive: false,
         plugins: {
 　			legend: {
+				// 凡例(ラベル)の非表示
 　　				display: false,
 　			}
 		}
