0fa58ea6ebf9eee3f03835f94e1ad8be4706e397
YosukeKudo0510 <yosuke.kudo@rakus-partners.co.jp>
2022-09-29 14:57:15 +0900

    さらに棒グラフの設定を追加

--- src/main/resources/static/chart/bar.js
+++ src/main/resources/static/chart/bar.js
@@ -6,19 +6,42 @@ window.onload = function () {
 	var data = {
 		labels: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
     	datasets: [{
-        label: '得点',
-        data: [880, 740, 900, 520, 930, 880, 740, 900, 520, 930, 880, 740],
-        backgroundColor: 'rgba(255, 100, 100, 1)'
-    	}]
+        	label: '支出',
+        	data: [880, 740, 900, 520, 930, 880, 740, 900, 520, 930, 880, 740],
+        	backgroundColor: 'rgba(255, 100, 100, 1)'
+    	},
+    	{
+        	label: '収入',
+        	data: [880, 740, 900, 520, 930, 880, 740, 900, 520, 930, 880, 740],
+        	backgroundColor: 'rgba(100, 100, 255, 1)'
+    	}
+    	]
 	};
 	
 	var options = {
 		scales: {
-			yAxes: [{
-				ticks: {
-                min: 300
+			xAxes: [{
+            	scaleLabel: {
+                	display: true,
+                	labelString: '月'
+            	}
+        	}],
+        	yAxes: [{
+            	ticks: {
+                	min: 0,
+                	userCallback: function(tick) {
+                    	return tick.toString() + '円';
+                	}
+            	},
+            	scaleLabel: {
+                	display: true,
+                	labelString: '金額'
             	}
         	}]
+    	},
+    	title: {
+        	display: true,
+        	text: '支出と収入の推移'
     	}
 	};
 	
