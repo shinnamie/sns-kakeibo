21d42ca47d35a5a670aff4cb4ef30594d36af762
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-09 15:58:32 +0900

    repositoryテスト(正常系)

---
+++ src/test/java/com/example/repository/kakeibo/KakeiboMapperPostKakeiboByYearAndMonthTest.java
@@ -0,0 +1,74 @@
+package com.example.repository.kakeibo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.domain.kakeibo.ExpenseItem;
+import com.example.domain.kakeibo.Kakeibo;
+import com.example.domain.kakeibo.Settlement;
+
+@SpringBootTest
+class KakeiboMapperPostKakeiboByYearAndMonthTest {
+	
+	@Autowired
+	private KakeiboMapper kakeiboMapper;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	// 正常系
+
+	@Test
+	@DisplayName("2022年8月が入力された時、11件の結果を返す")
+	void testFindKakeiboByYearAndMonth() throws Exception {
+		List<Kakeibo> kakeiboList = kakeiboMapper.findKakeiboByYearAndMonth("2022", "8");
+		assertEquals(11, kakeiboList.size());
+	}
+	
+	@Test
+	@DisplayName("2022年のみ入力された時、16件の結果を返す")
+	void testMontIsNull() throws Exception {
+		List<Kakeibo> kakeiboList = kakeiboMapper.findKakeiboByYearAndMonth("2022", "");
+		assertEquals(16, kakeiboList.size());
+	}
+	
+	@Test
+	@DisplayName("該当年月がない時、0件の結果を返す")
+	void testNotYearAndMonth() throws Exception {
+		List<Kakeibo> kakeiboList = kakeiboMapper.findKakeiboByYearAndMonth("2000", "1");
+		assertEquals(0, kakeiboList.size());
+	}
+	
+	@Test
+	@DisplayName("該当年がない時、0件の結果を返す")
+	void testNotYear() throws Exception {
+		List<Kakeibo> kakeiboList = kakeiboMapper.findKakeiboByYearAndMonth("2000", "");
+		assertEquals(0, kakeiboList.size());
+	}
+}
