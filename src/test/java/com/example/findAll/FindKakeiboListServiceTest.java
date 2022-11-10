0520f62058b925f3532047743c68e0a8ed8d79ce
Keigo Yamamoto <keigo.yamamoto@rakus-partners.co.jp>
2022-11-10 15:31:27 +0900

    家計簿一覧表示テスト（サービス）

--- src/test/java/com/example/findAll/FindKakeiboListServiceTest.java
+++ src/test/java/com/example/findAll/FindKakeiboListServiceTest.java
@@ -1,7 +1,6 @@
 package com.example.findAll;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.domain.kakeibo.Kakeibo;
+import com.example.repository.kakeibo.KakeiboMapper;
 import com.example.service.kakeibo.KakeiboService;
 
 
@@ -20,14 +20,25 @@ class FindKakeiboListServiceTest {
 
 	@Autowired
 	KakeiboService service;
-	
+
+	@Autowired
+	KakeiboMapper mapper;
+
 	@Test
-	@DisplayName("家計簿一覧表示のテスト")
+	@DisplayName("家計簿一覧表示のテスト（正常）")
 	void testKakeiboList() throws Exception {
-		List<Kakeibo> kakeiboList = service.findKakeiboList();
-	        int expect = kakeiboList.size();
-	        int actual = kakeiboList.size();
-	        assertEquals(expect, actual);
+		List<Kakeibo> kakeiboList = mapper.findAll();
+
+		//正常処理
+		assertThat(kakeiboList).isNotNull();   
 	}
 
+	@Test
+	@DisplayName("家計簿一覧表示のテスト（異常）")
+	void testKakeiboList2() throws Exception {
+		List<Kakeibo> kakeiboList2 = null;
+
+		//異常処理
+		assertThat(kakeiboList2).isNotNull();
+	}
 }
