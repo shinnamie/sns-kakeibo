bb68d67778e1ca2d8e6fc6e2adf71fc86cac1308
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-10 13:52:52 +0900

    バリデーションの追加

--- src/main/java/com/example/form/kakeibo/SearchKakeiboForm.java
+++ src/main/java/com/example/form/kakeibo/SearchKakeiboForm.java
@@ -2,6 +2,8 @@ package com.example.form.kakeibo;
 
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.validator.constraints.Range;
+
 import lombok.Data;
 
 @Data
@@ -9,6 +11,7 @@ public class SearchKakeiboForm {
 
 	/** 年 */
 	@NotBlank
+	@Range(min = 1950, max = 2050)
 	private String year;
 	
 	/** 月 */
