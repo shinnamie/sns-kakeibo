0024a136fe8874bf1ef878d240de6a2a3eaf8f51
shinnamie <shin.namie@rakus-partners.co.jp>
2022-11-18 13:01:24 +0900

    コントローラー収支内訳表示テスト（異常系）実装

--- src/main/java/com/example/controller/kakeibo/KakeiboController.java
+++ src/main/java/com/example/controller/kakeibo/KakeiboController.java
@@ -209,20 +209,22 @@ public class KakeiboController {
 	 * @return
 	 */
 	@RequestMapping(path = "{date}", method = RequestMethod.GET)
-	public String getBreakDown(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
+	public String getBreakDown(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)  @ModelAttribute @PathVariable("date") LocalDate date,
 			Model model) {
 
-		System.out.println(date);
 		// 月初日と月末日を取得する
 		String firstDateAndLastDate = kakeiboService.getFirstDayAndLastDay(date);
 		model.addAttribute("firstDateAndLastDate", firstDateAndLastDate);
+//		model.addAttribute("date", date);
 
 		// 年と月をString型で取得
 		String yearAndMonth = date.format(DateTimeFormatter.ofPattern("yyyy-MM"));
+		
 
 		// 選択されている年月の費目別の支出・収入を算出する
 		List<Kakeibo> kakeiboList = kakeiboService.totalByIncomeAndExpenditureBreakdown(yearAndMonth);
-		if (kakeiboList == null) { // データが存在しない場合
+		System.out.println(kakeiboList);
+		if (kakeiboList == null || kakeiboList.size() == 0) { // データが存在しない場合
 			model.addAttribute("message", "該当月のデータが存在しません。");
 			return "kakeibo/breakdown-income-balance";
 		} else {
@@ -232,15 +234,12 @@ public class KakeiboController {
 
 			// 総収入・総支出・収支のMapを取得
 			Map<String, Integer> totalAmountMap = kakeiboService.totalAmountMap(kakeiboMap);
-			System.out.println(totalAmountMap);
 
 			// Map内の費目別の割合を計算 Map<費目名,割合>
 			Map<String, Double> rateMap = kakeiboService.culcRate(kakeiboService.integerToDouble(kakeiboMap));
-			System.out.println(rateMap);
 
 			// 費目の総支出を格納したMapを呼び出す Map<費目名,支出額>
 			Map<String, Integer> itemExpenceMap = kakeiboService.itemExpenseMap(kakeiboMap);
-			System.out.println(itemExpenceMap);
 
 			// スコープに格納
 			model.addAttribute("totalAmountMap", totalAmountMap);
--- src/test/java/com/example/controller/kakeibo/KakeiboControllerTest.java
+++ src/test/java/com/example/controller/kakeibo/KakeiboControllerTest.java
@@ -1,12 +1,19 @@
 package com.example.controller.kakeibo;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
@@ -15,7 +22,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,7 +35,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.domain.kakeibo.ExpenseItem;
+import com.example.domain.kakeibo.Kakeibo;
+import com.example.domain.kakeibo.Settlement;
 import com.example.service.kakeibo.KakeiboService;
+
+//import org.junit.runner.RunWith;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,8 +49,12 @@ class KakeiboControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@Mock
+	@MockBean
 	private KakeiboService service;
+	
+	@InjectMocks
+	private KakeiboController controller;
+	
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -52,29 +72,81 @@ class KakeiboControllerTest {
 	void tearDown() throws Exception {
 	}
 
+//	@Test
+//	@DisplayName("収支内訳表示のテスト")
+//	void testBreakdownIncomeBalance() throws Exception{
+//		
+//		Map<String,Integer> totalAmountMap = new HashMap<>();
+//		totalAmountMap.put("総収入", 0);
+//		totalAmountMap.put("総支出", 128465);
+//		totalAmountMap.put("収支", -1288465);
+//		
+//		Map<String,Integer> itemExpenceMap = new HashMap<>();
+//		itemExpenceMap.put("税・社会保障", 29000);
+//		itemExpenceMap.put("美容", 3000);
+//		itemExpenceMap.put("健康・医療", 4550);
+//		itemExpenceMap.put("衣服", 1220);
+//		itemExpenceMap.put("水道・光熱費", 8000);
+//		itemExpenceMap.put("自動車", 12245);
+//		itemExpenceMap.put("食費", 5450);
+//		itemExpenceMap.put("住宅", 6500);
+//		
+//		Map<String,Double> rateMap = new HashMap<>();
+//		rateMap.put("税・社会保障", 22.57);
+//		rateMap.put("美容", 2.34);
+//		rateMap.put("健康・医療", 3.54);
+//		rateMap.put("衣服", 0.95);
+//		rateMap.put("水道・光熱費", 6.23);
+//		rateMap.put("自動車", 9.53);
+//		rateMap.put("食費", 4.24);
+//		rateMap.put("住宅", 50.6);
+//		
+//		MvcResult result = mockMvc.perform(get("/kakeibo/breakdown-income-balance")
+//				.param("date","2022-08-16"))
+//				// ステータスコードがOK(200)であるかのテスト
+//				.andExpect(status().isOk())
+//				// 指定されたHTMLを表示できているかのテスト
+//				.andExpect(view().name("kakeibo/breakdown-income-balance"))
+//				.andReturn();
+//		
+////		doReturn(null).when(service).totalIncomeAndExpenditureBreakdown(anyString());
+//		ModelAndView mavTotalAmountMap = result.getModelAndView();
+//		assertEquals(mavTotalAmountMap.getModel().get("totalAmountMap"),totalAmountMap);
+//		
+////		ModelAndView mavMonthlyBalanceCalculationMap = result.getModelAndView();
+////		assertEquals(mavMonthlyBalanceCalculationMap.getModel().get("itemExpenceMap"));
+////		
+////		ModelAndView mavDateMap = result.getModelAndView();
+////		assertEquals(mavDateMap.getModel().get("rateMap"));
+////		
+////		ModelAndView mavfirst = result.getModelAndView();
+////		assertEquals(firstDateAndLastDate,mavDateMap.getModel().get("firstDateAndLastDate"));
+//		
+//	}
+	
 	@Test
-	@DisplayName("収支内訳表示のテスト")
-	void testBreakdownIncomeBalance() throws Exception{
-		//mapにテスト用の値を格納
+	@DisplayName("異常系：収支内訳表示のテスト")
+	void testBreakdownIncomeBalanceNull() throws Exception{
 		
-		when(service.totalByIncomeAndExpenditureBreakdown("2022-08")).thenReturn(breakdownMap);
+		LocalDate date = LocalDate.now();
+		doReturn("2022/11/01 - 2022/11/30").when(service).getFirstDayAndLastDay(date);
 		
-		MvcResult result = mockMvc.perform(get("/kakeibo/breakdown-income-balance")
-				.param("date","2022-08-16"))
+		String yearAndMonth = date.format(DateTimeFormatter.ofPattern("yyyy-MM"));
+		List<Kakeibo> kakeiboList = null;
+		when(service.totalByIncomeAndExpenditureBreakdown(yearAndMonth)).thenReturn(kakeiboList);
+		
+		MvcResult result = mockMvc.perform(get("/kakeibo/breakdown")
+				.param("date","2022-11-16"))
 				// ステータスコードがOK(200)であるかのテスト
 				.andExpect(status().isOk())
 				// 指定されたHTMLを表示できているかのテスト
 				.andExpect(view().name("kakeibo/breakdown-income-balance"))
 				.andReturn();
 		
-		ModelAndView mavItemMap = result.getModelAndView();
-		assertEquals(mavItemMap.getModel().get("totalAmountMap"));
-		
-		ModelAndView mavMonthlyBalanceCalculationMap = result.getModelAndView();
-		assertEquals(mavMonthlyBalanceCalculationMap.getModel().get("itemExpenceMap"));
-		
-		ModelAndView mavDateMap = result.getModelAndView();
-		assertEquals(mavDateMap.getModel().get("rateMap"));
+		ModelAndView mavTotalAmountMap = result.getModelAndView();
+		ModelAndView message = result.getModelAndView();
+		assertEquals("2022/11/01 - 2022/11/30",mavTotalAmountMap.getModel().get("firstDateAndLastDate"));
+		assertEquals("該当月のデータが存在しません。",message.getModel().get("message"));
 		
 	}
 
