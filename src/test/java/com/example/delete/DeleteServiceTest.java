c1b148d47c6b912597da294754755be3116d03c7
Keigo Yamamoto <keigo.yamamoto@rakus-partners.co.jp>
2022-11-18 17:04:49 +0900

    (再々修正)削除機能のサービステスト

--- src/test/java/com/example/delete/DeleteServiceTest.java
+++ src/test/java/com/example/delete/DeleteServiceTest.java
@@ -1,30 +1,54 @@
 package com.example.delete;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.domain.kakeibo.Kakeibo;
 import com.example.repository.kakeibo.KakeiboMapper;
 import com.example.service.kakeibo.KakeiboService;
 
 @SpringBootTest
 public class DeleteServiceTest {
-
+	
 	@InjectMocks
 	KakeiboService service;
-
+	
 	@Mock
 	KakeiboMapper mapper;
-
+	
 	@Test
-	@DisplayName("家計簿の削除テスト")
-	void testKakeibodelete() throws Exception{
+	@DisplayName("家計簿の削除テスト（正常）")
+	void testKakeiboDelete() throws Exception{
+		Kakeibo kakeibo = new Kakeibo();
+		kakeibo.setId(1);
+		
+		
+		when(mapper.delete(anyLong())).thenReturn(true);
+		
+		boolean actual = mapper.delete(kakeibo.getId());
+		
+		assertTrue(actual);
+		
 
-		mapper.delete(1L);
-		Mockito.verify(mapper).delete(1L);
-
+	}
+	@Test
+	@DisplayName("家計簿の削除テスト（異常）")
+	void testKakeiboDeleteFalse() throws Exception{
+		Kakeibo kakeibo = new Kakeibo();
+		kakeibo.setId(1);
+		
+	when(mapper.delete(anyLong())).thenReturn(false);
+		
+		boolean actual = mapper.delete(kakeibo.getId());
+		
+		assertFalse(actual);
 	}
 }
