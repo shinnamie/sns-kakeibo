8b8977b00a348a77e9d2e37756947b18dcfa090f
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-25 16:38:45 +0900

    Controller側での入力値チェック

--- src/main/java/com/example/controller/user/LoginController.java
+++ src/main/java/com/example/controller/user/LoginController.java
@@ -5,7 +5,10 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +25,11 @@ public class LoginController {
 	
 	@Autowired
 	private HttpSession session;
+	
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
 	
 	/**
 	 * ログイン画面に遷移(user/login.html)
@@ -44,7 +52,12 @@ public class LoginController {
 	 * @return
 	 */
 	@PostMapping("/login")
-	public String postLogin(LoginForm loginForm, Model model) {
+	public String postLogin(@Validated LoginForm loginForm, BindingResult result, Model model) {
+		
+		// 入力値チェック
+		if (result.hasErrors()) {
+			return getLogin();
+		}
 		
 		User user = service.login(loginForm.getMailAdress(), loginForm.getPassword());
 		
