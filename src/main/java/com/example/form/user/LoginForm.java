cefe8dc7aa2ce8ff2fcf64a81d63c080e6619d97
keisukehasegawa0918 <chiro9689@gmail.com>
2022-11-25 15:50:03 +0900

    入力値チェックに関する項目実装

--- src/main/java/com/example/form/user/LoginForm.java
+++ src/main/java/com/example/form/user/LoginForm.java
@@ -1,11 +1,17 @@
 package com.example.form.user;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class LoginForm {
 
+	@NotBlank
+	@Email
 	private String mailAdress;
+	@NotBlank
 	private String password;
 
 }
