55fdcc58158017f54fa4f13a1cebbbcb4fb3746c
Keigo Yamamoto <keigo.yamamoto@rakus-partners.co.jp>
2022-11-25 09:10:26 +0900

    UML図の画像ファイル追加/lombok対応用リファクタリング

--- src/main/java/com/example/domain/kakeibo/Kakeibo.java
+++ src/main/java/com/example/domain/kakeibo/Kakeibo.java
@@ -3,6 +3,9 @@ package com.example.domain.kakeibo;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 
+import lombok.Data;
+
+@Data
 public class Kakeibo {
 
 	/** 家計簿Id */
@@ -31,93 +34,7 @@ public class Kakeibo {
 	private ExpenseItem expenseItem;
 	/** 決済 */
 	private Settlement settlement;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-	public LocalDate getPaymentDate() {
-		return paymentDate;
-	}
-	public void setPaymentDate(LocalDate paymentDate) {
-		this.paymentDate = paymentDate;
-	}
-	public Integer getExpenseItemId() {
-		return expenseItemId;
-	}
-	public void setExpenseItemId(Integer expenseItemId) {
-		this.expenseItemId = expenseItemId;
-	}
-	public Integer getExpenditureAmount() {
-		return expenditureAmount;
-	}
-	public void setExpenditureAmount(Integer expenditureAmount) {
-		this.expenditureAmount = expenditureAmount;
-	}
-	public Integer getIncomeAmount() {
-		return incomeAmount;
-	}
-	public void setIncomeAmount(Integer incomeAmount) {
-		this.incomeAmount = incomeAmount;
-	}
-	public Integer getSettlementId() {
-		return settlementId;
-	}
-	public void setSettlementId(Integer settlementId) {
-		this.settlementId = settlementId;
-	}
-	public String getUsedStore() {
-		return usedStore;
-	}
-	public void setUsedStore(String usedStore) {
-		this.usedStore = usedStore;
-	}
-	public String getRemarks() {
-		return remarks;
-	}
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
-	}
-	public Timestamp getInsertAt() {
-		return insertAt;
-	}
-	public void setInsertAt(Timestamp insertAt) {
-		this.insertAt = insertAt;
-	}
-	public Timestamp getUpdateAt() {
-		return updateAt;
-	}
-	public void setUpdateAt(Timestamp updateAt) {
-		this.updateAt = updateAt;
-	}
-
-	public ExpenseItem getExpenseItem() {
-		return expenseItem;
-	}
-	public void setExpenseItem(ExpenseItem expenseItem) {
-		this.expenseItem = expenseItem;
-	}
-	public Settlement getSettlement() {
-		return settlement;
-	}
-	public void setSettlement(Settlement settlement) {
-		this.settlement = settlement;
-	}
-	@Override
-	public String toString() {
-		return "Kakeibo [id=" + id + ", userId=" + userId + ", paymentDate=" + paymentDate + ", expenseItemId="
-				+ expenseItemId + ", expenditureAmount=" + expenditureAmount + ", incomeAmount=" + incomeAmount
-				+ ", settlementId=" + settlementId + ", usedStore=" + usedStore + ", remarks=" + remarks + ", insertAt="
-				+ insertAt + ", updateAt=" + updateAt + ", expenseItem=" + expenseItem
-				+ ", settlement=" + settlement + "]";
-	}
+	
 
 
 }
