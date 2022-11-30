ddcf5a7c64ec20cb3aac394079234c4dd3acd1fb
shinnamie <shin.namie@rakus-partners.co.jp>
2022-11-30 11:49:45 +0900

    repositroyの正常系テスト実装

--- src/main/java/com/example/repository/user/SignUpMapper.java
+++ src/main/java/com/example/repository/user/SignUpMapper.java
@@ -4,9 +4,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.example.domain.user.User;
 
-@Mapper
-public interface SignUpMapper {
-
-	boolean signUp(User user);
-
-}
+//@Mapper
+//public interface SignUpMapper {
+//
+//	boolean signUp(User user);
+//
+//}
---
+++ src/test/java/com/example/repository/user/LoginMapperTest.java
@@ -0,0 +1,56 @@
+package com.example.repository.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.domain.user.User;
+
+@SpringBootTest
+class LoginMapperTest {
+	
+	@Autowired
+	LoginMapper loginMapper;
+			
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@Test
+	@DisplayName("正常系：入力されたメールアドレスとパスワードが適当であればUser情報を返す")
+	void testLogin() throws Exception {
+		
+		//期待するUser情報を生成
+		User expectedUser = new User();
+		expectedUser.setId(1L);
+		expectedUser.setMailAddress("aaa@aaa");
+		expectedUser.setPassword("test");
+		
+		//mapperを通したUserを生成
+		User mapperUser = loginMapper.Login("aaa@aaa", "test");
+		
+		assertEquals(expectedUser.getId(), mapperUser.getId());
+		assertEquals(expectedUser.getMailAddress(), mapperUser.getMailAddress());
+		assertEquals(expectedUser.getPassword(), mapperUser.getPassword());
+	}
+
+}
