71c5c3d47fc4dbd425d1975b645b0db56d48fc62
shinnamie <shin.namie@rakus-partners.co.jp>
2022-11-30 13:29:21 +0900

    repositoryテスト実装

--- src/test/java/com/example/repository/user/LoginMapperTest.java
+++ src/test/java/com/example/repository/user/LoginMapperTest.java
@@ -15,10 +15,10 @@ import com.example.domain.user.User;
 
 @SpringBootTest
 class LoginMapperTest {
-	
+
 	@Autowired
 	LoginMapper loginMapper;
-			
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -34,23 +34,45 @@ class LoginMapperTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	@DisplayName("正常系：入力されたメールアドレスとパスワードが適当であればUser情報を返す")
 	void testLogin() throws Exception {
-		
-		//期待するUser情報を生成
+
+		// 期待するUser情報を生成
 		User expectedUser = new User();
 		expectedUser.setId(1L);
 		expectedUser.setMailAddress("aaa@aaa");
 		expectedUser.setPassword("test");
-		
-		//mapperを通したUserを生成
+
+		// mapperを通したUserを生成
 		User mapperUser = loginMapper.Login("aaa@aaa", "test");
-		
+
 		assertEquals(expectedUser.getId(), mapperUser.getId());
 		assertEquals(expectedUser.getMailAddress(), mapperUser.getMailAddress());
 		assertEquals(expectedUser.getPassword(), mapperUser.getPassword());
+	}
+
+	@Test
+	@DisplayName("異常系：入力されたメールアドレスが無効な場合nullを返す")
+	void testLoginErrorMailAddress() throws Exception {
+
+		// メールアドレスが無効な状態でmapperを通したUserを生成
+		User mapperUser = loginMapper.Login("a@a", "test");
+
+		assertNull(mapperUser);
+
+	}
+
+	@Test
+	@DisplayName("異常系：入力されたパスワードが無効な場合nullを返す")
+	void testLoginErrorPassword() throws Exception {
+
+		// メールアドレスが無効な状態でmapperを通したUserを生成
+		User mapperUser = loginMapper.Login("aaa@aaa", "miss");
+
+		assertNull(mapperUser);
+
 	}
 
 }
