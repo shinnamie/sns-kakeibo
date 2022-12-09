9ed3b34c5ecaf33941cbbc7bb6b7e962fd818efd
keisukehasegawa0918 <chiro9689@gmail.com>
2022-12-09 09:22:22 +0900

    最新の投稿順になるよう修正、メソッドの削除

--- src/main/java/com/example/service/post/impl/PostServiceImpl.java
+++ src/main/java/com/example/service/post/impl/PostServiceImpl.java
@@ -1,8 +1,5 @@
 package com.example.service.post.impl;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,21 +17,12 @@ public class PostServiceImpl implements PostService {
 	@Autowired
 	private PostMapper postMapper;
 	
+	/*
+	 * 掲示板IDでトピックごとの投稿一覧を取得
+	 */
 	@Override
 	public List<Post> selectPostList(Long boardId) {
 		return postMapper.findPostList(boardId);
 	}
 	
-	/*
-	 * 型変換
-	 * Timestamp型 →　String型(yyyy/MM/dd HH:mm:ss 表示)
-	 */
-	public String changeType(LocalDateTime localDateTime) {
-		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-		String stringTime = dtf.format((TemporalAccessor) localDateTime);
-		
-		return stringTime;
-	}
-
 }
