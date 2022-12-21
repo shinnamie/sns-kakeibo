9f6e02eef99de636c70a7398bae0cc7283cffaa8
shinnamie <shin.namie@rakus-partners.co.jp>
2022-12-21 10:20:37 +0900

    mapper作成

--- src/main/java/com/example/repository/board/BoardMapper.java
+++ src/main/java/com/example/repository/board/BoardMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.example.domain.board.Board;
+import com.example.domain.post.Post;
 
 @Mapper
 public interface BoardMapper {
@@ -15,5 +16,21 @@ public interface BoardMapper {
 	 */
 
 	public List<Board> findBoardList();
+	
+	/**
+	 * board新規作成
+	 * @param Board
+	 * @return boolean
+	 *
+	 * */
+	public boolean saveBoard(Board board) throws Exception;
+
+	/**
+	 * board削除
+	 * @param Board
+	 * @return boolean
+	 *
+	 * */
+	public boolean deleteBoard(Board board) throws Exception;
 
 }
